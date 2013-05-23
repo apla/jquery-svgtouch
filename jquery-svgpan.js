97044d31143d3914f66430cc317decb7447b1af9
Pawel Sledzikowski <pawel.sledzikowski@gmail.com>
2013-05-24 01:19:37 +0200

    Add multi-touch support
    Implement initial multi-touch support:
    - Touch panning
    - Multi-Touch zooming (via pinch to zoom gesture)

--- jquery-svgpan.js
+++ jquery-svgpan.js
@@ -35,18 +35,20 @@
    implied, of John Krauss.
 **/
 
-// SVGPan library 1.2.2 license and documentation:
+// SVGPan library 1.3 license and documentation:
 
-/** 
- *  SVGPan library 1.2.2
+/**
+ *  SVGPan library 1.3
  * ======================
  *
- * Given an unique existing element with id "viewport" (or when missing, the 
- * first g-element), including the the library into any SVG adds the following 
+ * Given an unique existing element with id "viewport" (or when missing, the
+ * first g-element), including the the library into any SVG adds the following
  * capabilities:
  *
  *  - Mouse panning
  *  - Mouse zooming (using the wheel)
+ *  - Touch panning
+ *  - Multi-Touch zooming (using two figer pinch to zoom gesture)
  *  - Object dragging
  *
  * You can configure the behaviour of the pan/zoom/drag with the variables
@@ -57,6 +59,9 @@
  *  - Zooming (while panning) on Safari has still some issues
  *
  * Releases:
+ *
+ * 1.3, Fri May 24 23:12:05 GMT 2013, Pawel Sledzikowski
+ *    Added multi-touch support
  *
  * 1.2.2, Tue Aug 30 17:21:56 CEST 2011, Andrea Leofreddi
  *    - Fixed viewBox on root tag (#7)
@@ -80,17 +85,17 @@
  * This code is licensed under the following BSD license:
  *
  * Copyright 2009-2010 Andrea Leofreddi <a.leofreddi@itcharm.com>. All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modification, are
  * permitted provided that the following conditions are met:
- * 
+ *
  *    1. Redistributions of source code must retain the above copyright notice, this list of
  *       conditions and the following disclaimer.
- * 
+ *
  *    2. Redistributions in binary form must reproduce the above copyright notice, this list
  *       of conditions and the following disclaimer in the documentation and/or other materials
  *       provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY Andrea Leofreddi ``AS IS'' AND ANY EXPRESS OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Andrea Leofreddi OR
@@ -100,7 +105,7 @@
  * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * The views and conclusions contained in the software and documentation are those of the
  * authors and should not be interpreted as representing official policies, either expressed
  * or implied, of Andrea Leofreddi.
@@ -122,7 +127,7 @@
     var NONE = 0,
         PAN = 1,
         DRAG = 2,
-        init = function (root, svgRoot, enablePan, enableZoom, enableDrag, zoomScale) {
+        init = function (root, svgRoot, enablePan, enableZoom, enableDrag, enableTouch, zoomScale) {
 
             var state = NONE,
                 stateTarget,
@@ -132,6 +137,7 @@
                 $root = $(root),
                 $parent = $root.parent(),
                 recentOffset = $root.offset(),
+                startTouches = [],
 
                 // FF sometimes doesn't calculate this anything near correctly
                 // for SVGs.
@@ -145,6 +151,44 @@
                     var s = "[ " + matrix.a + ", " + matrix.c + ", " + matrix.e + "\n  " + matrix.b + ", " + matrix.d + ", " + matrix.f + "]";
 
                     return s;
+                },
+
+                /**
+                 * Gets distance between two point touches.
+                 */
+                getDistance = function (touch1, touch2) {
+                    var x = touch2.pageX - touch1.pageX,
+                        y = touch2.pageY - touch1.pageY;
+                    return Math.sqrt((x*x) + (y*y));
+                },
+
+                /**
+                 * Update touch coordinates based on some other touch coordinates.
+                 */
+                updateTouch = function (dstTouch, srcTouch) {
+                  dstTouch.pageX = srcTouch.pageX;
+                  dstTouch.pageY = srcTouch.pageY;
+                },
+
+                /**
+                 * Instance an SVGPoint object with given touch event coordinates.
+                 */
+                getTouchEventPoint = function (evt) {
+                  var p = root.createSVGPoint(),
+                      targetTouches = evt.targetTouches,
+                      offsetX,
+                      offsetY;
+
+                  if (targetTouches.length) {
+                    var touch = targetTouches[0];
+                    offsetX = touch.pageX;
+                    offsetY = touch.pageY;
+                  }
+
+                  p.x = offsetX;
+                  p.y = offsetY;
+
+                  return p;
                 },
 
                 /**
@@ -177,6 +221,109 @@
                     var s = "matrix(" + matrix.a + "," + matrix.b + "," + matrix.c + "," + matrix.d + "," + matrix.e + "," + matrix.f + ")";
 
                     element.setAttribute("transform", s);
+                },
+
+                /**
+                 * Handle touch start event.
+                 */
+                handleTouchStart = function (evt) {
+                  var g = svgRoot;
+
+                  stateTf = g.getCTM().inverse();
+
+                  stateOrigin = getTouchEventPoint(evt).matrixTransform(stateTf);
+
+                  var touches = evt.touches;
+                  // Check if there are two or more touches (fingers) and in
+                  // case so store copy of all of them for future reference
+                  // in pinching (zooming) functionality.
+                  if (touches.length >= 2) {
+                    touches.forEach(function (touch) {
+                      var found = false;
+                      startTouches.forEach(function (startTouch) {
+                        if (touch.identifier === startTouch.identifier) {
+                          found = true;
+                          return;
+                        }
+                      });
+                      if (!found) {
+                        var touchCopy = $.extend({}, touch);
+                        startTouches.push(touchCopy);
+                      }
+                    });
+                  }
+
+                  evt.preventDefault();
+                },
+
+                /**
+                 * Handle touch end event.
+                 */
+                handleTouchEnd = function (evt) {
+                  // Remove ending touches from internal helper collection
+                  // related to pinch to zoom gesture.
+                  evt.changedTouches.forEach(function (changedTouch) {
+                    startTouches.forEach(function (startTouch) {
+                      if (startTouch.identifier === changedTouch.identifier) {
+                        var idx = startTouches.indexOf(startTouch);
+                        startTouches.splice(idx, 1);
+                      }
+                    });
+                  });
+
+                  evt.preventDefault();
+                },
+
+                /**
+                 * Handle touch move event.
+                 */
+                handleTouchMove = function (evt) {
+                  evt.preventDefault();
+
+                  var g = svgRoot,
+                      touches = evt.touches,
+                      z,
+                      p,
+                      k;
+
+                  // Handle zoom while 2 fingers
+                  if (touches.length >= 2 && enableZoom) {
+                    var touch0 = touches[0]
+                      , touch1 = touches[1];
+
+                    var startTouch0 = startTouches[0];
+                    var startTouch1 = startTouches[1];
+
+                    z = getDistance(touch0, touch1) / getDistance(startTouch0, startTouch1);
+
+                    p = root.createSVGPoint();
+                    p.x = (touch0.pageX + touch1.pageX) / 2;
+                    p.y = (touch0.pageY + touch1.pageY) / 2;
+
+                    p = p.matrixTransform(g.getCTM().inverse());
+
+                    // Compute new scale matrix in current mouse position
+                    k = root.createSVGMatrix().translate(p.x, p.y).scale(z).translate(-p.x, -p.y);
+
+                    setCTM(g, g.getCTM().multiply(k));
+
+                    if (typeof stateTf === "undefined") {
+                        stateTf = g.getCTM().inverse();
+                    }
+
+                    stateTf = stateTf.multiply(k.inverse());
+
+                    // Update future starting touch coordinates with present
+                    // values.
+                    updateTouch(startTouch0, touch0);
+                    updateTouch(startTouch1, touch1);
+                  } else if (!startTouches.length && enablePan) {
+                    // Panning is disabled while simultaneously pinch to
+                    // zoom gesture is activated.
+                    p = getTouchEventPoint(evt).matrixTransform(stateTf);
+
+                    setCTM(g, stateTf.inverse().translate(p.x - stateOrigin.x, p.y - stateOrigin.y));
+                  }
                 },
 
                 /**
@@ -341,6 +488,12 @@
             window.addEventListener('mousewheel', handleMouseWheel, false); // Chrome/Safari/others
             window.addEventListener('DOMMouseScroll', handleMouseWheel, false); // Firefox
 
+            if (enableTouch) {
+                root.addEventListener('touchstart', handleTouchStart, false);
+                root.addEventListener('touchend', handleTouchEnd, false);
+                root.addEventListener('touchmove', handleTouchMove, false);
+            }
+
         };
 
     /**
@@ -352,10 +505,11 @@
        @param enableDrag Boolean enable or disable dragging (default disabled)
        @param zoomScale Float zoom sensitivity, defaults to .2
     **/
-    $.fn.svgPan = function (viewportId, enablePan, enableZoom, enableDrag, zoomScale) {
+    $.fn.svgPan = function (viewportId, enablePan, enableZoom, enableDrag, enableTouch, zoomScale) {
         enablePan = typeof enablePan !== 'undefined' ? enablePan : true;
         enableZoom = typeof enableZoom !== 'undefined' ? enableZoom : true;
         enableDrag = typeof enableDrag !== 'undefined' ? enableDrag : false;
+        enableTouch = typeof enableTouch !== 'undefined' ? enableTouch : true;
         zoomScale = typeof zoomScale !== 'undefined' ? zoomScale : 0.2;
 
         return $.each(this, function (i, el) {
@@ -366,7 +520,7 @@
             if ($el.is('svg') && $el.data('SVGPan') !== true) {
                 viewport = $el.find('#' + viewportId)[0];
                 if (viewport) {
-                    init($el[0], viewport, enablePan, enableZoom, enableDrag, zoomScale);
+                    init($el[0], viewport, enablePan, enableZoom, enableDrag, enableTouch, zoomScale);
                 } else {
                     throw "Could not find viewport with id #" + viewportId;
                 }
